0d91d7b16b7121d5578d504f016eb70df63e92ec
arc-1409 <lulu.august.liao@gmail.com>
2025-04-04 22:45:27 -0400

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+    private int id;
+
+    public Animal(int thisID) {
+        thisID = id;
+    }
+
+    public void makeSound() {
+        System.out.println("Animal makes sound.");
+    }
+
+    public void eat() {
+        System.out.println("Animal eats.");
+    }
+}
